50c31724bc2e09804e4b189d284f7a4636c0d7ef
naresh35442 <33330608+naresh35442@users.noreply.github.com>
2018-06-09 19:25:11 -0400

    hi

---
+++ src/main/java/com/naresh/tutorial/maven/HelloMaven.java
@@ -0,0 +1,11 @@
+package com.naresh.tutorial.maven;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class HelloMaven {
+	@Test
+	public void test() {
+		Assert.assertEquals("Hello Maven", new String("Hello Maven"));
+	}
+}
